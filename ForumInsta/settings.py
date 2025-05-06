d0e2af678fc809c8fef8900287328a19f6efbadd
AbakarSilet <abakarix4@gmail.com>
2025-05-06 21:18:36 +0100

    Debut de deploiement

--- ForumInsta/settings.py
+++ ForumInsta/settings.py
@@ -276,6 +276,20 @@ if not DEBUG:
         },
     }
     
+    # Configuration AWS S3
+    AWS_ACCESS_KEY_ID = os.getenv('AWS_ACCESS_KEY_ID')
+    AWS_SECRET_ACCESS_KEY = os.getenv('AWS_SECRET_ACCESS_KEY')
+    AWS_STORAGE_BUCKET_NAME = os.getenv('AWS_STORAGE_BUCKET_NAME')
+    AWS_S3_SIGNATURE_NAME = 's3v4'
+    AWS_S3_REGION_NAME = 'eu-north-1'
+    AWS_S3_VERIFY = True
+    DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
+    AWS_S3_CUSTOM_DOMAIN = f'{AWS_STORAGE_BUCKET_NAME}.s3.amazonaws.com'
+    AWS_S3_FILE_OVERWRITE = False
+    AWS_DEFAULT_ACL = 'public-read'
+    
+    # Définition de MEDIA_URL pour qu'il pointe vers S3
+    MEDIA_URL = f'https://{AWS_S3_CUSTOM_DOMAIN}/'
     
     STATIC_URL = '/static/'
     STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
